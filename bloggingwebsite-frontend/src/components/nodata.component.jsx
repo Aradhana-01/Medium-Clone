68a371f5645e424b94cb68fb109403d4a6e38556
Aradhana-01 <133377525+Ar-D-coder@users.noreply.github.com>
2024-04-05 20:05:33 +0530

    frontend  files deleted

--- bloggingwebsite-frontend/src/components/nodata.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const NoDataMessage = ({message}) => {
-  return (
-    <div className='text-center w-full p-4 rounded-full bg-grey/50 mt-3'>
-        <p>{ message } </p>
-    </div>
-  )
-}
